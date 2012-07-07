2fa77c7a07849860d49a3252070122fae1866b78
Nick Desaulniers <ndesaulniers@mozilla.com>
2012-07-06 17:33:28 -0700

    initialized rails

---
+++ common_ground/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CommonGround::Application.config.secret_token = '237286f6356985cb36cec913b5ce21f858c9b00261fb72279582fd39df1f686941b0626fd23d8cfa8e1ae1be5462cacb5b03f71a27a29afdb5a5d4630c6b2cef'
