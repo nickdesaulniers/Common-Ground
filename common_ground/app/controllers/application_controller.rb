b82de5d334791bf106728793c30d01779a888e1a
David Zbarsky <dzbarsky@gmail.com>
2012-07-07 16:19:22 -0700

    add algorithm

--- common_ground/app/controllers/application_controller.rb
+++ common_ground/app/controllers/application_controller.rb
@@ -13,13 +13,57 @@ class ApplicationController < ActionController::Base
       end
     end
 
-    if @count > 0 
+    if @count > 0
       return [@lats/@count, @longs/@count]
     end
   end
 
   # Locations are in the form of [lat,long]
-  def rank(locations)
+  def rank(locations, center, userList)
+    @points = []
+    locations.each do |location|
+      @points.push({"id" => location.id, "lat" => location.lat, "lng" => location.lng})
+    end
 
+    #Compute time from each user to all points
+    userList.each do |user|
+      if user.latitude && user.longitude
+        request = {
+            "format" => "json",
+            "api_id" => API_ID,
+            "api_key" => API_KEY,
+            "origin" => { "lat" => user.latitude, "lng" => user.longitude },
+            "travel_time" => "36000",
+            "mode" => "driving",
+            "points" => @points
+          }.to_json
+        url = 'http://api.igeolise.com/time_filter'
+        user.times = ActiveSupport::JSON.decode(RestClient.post(url, request, :content_type => :json, :accept => :json))
+        user.times.each do |result|
+          @points.select { |p| p["id"] = result.id }.totalTime += result.time
+        end
+      else
+        user.times = [];
+      end
+    end
+
+    #Compute average time to each point
+    @points.each do |point|
+      point.averageTime = point.time / userList.count
+    end
+
+    #Compute sum of squares of the difference of usertime and averagetime
+    @points.each do |point|
+     @userList.each do |user|
+       timeForUser = user.times.select { |p| p["id"] = point.id}.time
+       point.totalSquareTime += (timeForUser - point.averageTime)^2
+     end
+    end
+
+
+    #Maybe check if place is open using google api?
+
+    #Sort points by min totalSquareTime
+    @points.sort! { |a,b| a.totalSquareTime <=> b.totalSquareTime }
   end
 end
